bc0cf993e8ccf7ee0627f18da80563d572a9c2db
piking77420 <bon.gabriel77@gmail.com>
2026-08-02 11:44:25 +0200

    Add ignore magic numbner for clang tidy in test files

--- Source/UnitTest/Vector4d_Test.cpp
+++ Source/UnitTest/Vector4d_Test.cpp
@@ -2,12 +2,18 @@
 
 #include <Vector4d.hpp>
 
+// NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers)
+
+using namespace MathLib;
+
 TEST(TestVec4, Constructor)
 {
-    MathLib::Vector4d v = MathLib::Vector4d(1., 2., 3., 4.);
+    const Vector4d v = Vector4d(1., 2., 3., 4.);
 
     EXPECT_DOUBLE_EQ(v.getX(), 1.0);
     EXPECT_DOUBLE_EQ(v.getY(), 2.0);
     EXPECT_DOUBLE_EQ(v.getZ(), 3.0);
     EXPECT_DOUBLE_EQ(v.getW(), 4.0);
 }
+
+// NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
