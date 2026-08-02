c7d40f324f63852e65bd011dec0001a6754e4257
piking77420 <bon.gabriel77@gmail.com>
2026-08-02 18:38:19 +0200

    Add cmake arm build instruction

--- cmake/MathlibEnableSimd.cmake
+++ cmake/MathlibEnableSimd.cmake
@@ -24,25 +24,41 @@ function(mathlib_enable_simd target level)
         endif()
 
     elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-        if(level_upper STREQUAL "SSE2")
+        if (CPU_ARM64)
+            if(level_upper STREQUAL "NEON")
+            # NEON is always available on ARM64/AArch64.
+            set(simd_option "")
+
+            elseif(level_upper STREQUAL "SVE")
+                set(simd_option -march=armv8.2-a+sve)
+
+            else()
+                message(FATAL_ERROR
+                    "Unsupported ARM64 SIMD level '${level}'. "
+                    "Supported levels: NEON, SVE"
+            )
+            endif()
+        endif()
+        if (CPU_X86_64)
+            if(level_upper STREQUAL "SSE2")
             set(simd_option -msse2)
 
-        elseif(level_upper STREQUAL "AVX")
-            set(simd_option -mavx)
+            elseif(level_upper STREQUAL "AVX")
+                set(simd_option -mavx)
 
-        elseif(level_upper STREQUAL "AVX2")
-            set(simd_option -mavx2)
+            elseif(level_upper STREQUAL "AVX2")
+                set(simd_option -mavx2)
 
-        elseif(level_upper STREQUAL "AVX512F")
-            set(simd_option -mavx512f)
+            elseif(level_upper STREQUAL "AVX512F")
+                set(simd_option -mavx512f)
 
-        else()
-            message(FATAL_ERROR
-                "Unsupported SIMD level '${level}' for "
-                "${CMAKE_CXX_COMPILER_ID}"
-            )
-        endif()
-
+            else()
+                message(FATAL_ERROR
+                    "Unsupported SIMD level '${level}' for "
+                    "${CMAKE_CXX_COMPILER_ID}"
+                )
+            endif()
+        endif()        
     else()
         message(FATAL_ERROR
             "Unsupported compiler: ${CMAKE_CXX_COMPILER_ID}"
