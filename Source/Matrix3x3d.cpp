ae33ab95aeb16ebef3bdd2f11d231f1d280de1e7
piking <bon.gabriel77@gmail.com>
2026-08-22 16:39:10 +0200

    Add matrix4x4d matrix3x3d simd avx , and Vector3d permutation and to Vector2d

--- Source/Matrix3x3d.cpp
+++ Source/Matrix3x3d.cpp
@@ -1,51 +1,51 @@
 #include <Matrix3x3d.hpp>
-#include <Vector2.hpp>
 #include <Matrix2x2d.hpp>
+#include <Vector2d.hpp>
+#include <Vector3d.hpp>
 
 namespace MathLib
 {
-    double Matrix3x3<double>::determinant() const noexcept
-    {
-        // [a, b, c]
-        // [d, e, f]
-        // [g, h, i]
-
-#if defined(SIMD_SSE2)
-        return 0.0;
-#else
-        // clang-format off
-        const double efhi = Matrix2x2d(getM22(), getM23(), 
-                                        getM32(), getM33()).determinant();
-
-        const double bchi = Matrix2x2d(getM12(), getM13(), 
-                                        getM32(), getM33()).determinant();
-
-        const double bcef = Matrix2x2d(getM12(), getM13(), 
-                                        getM22(), getM23()).determinant();
-        // clang-format on
-        const Vector3d adg = Vector3d(getM11(), -getM21(), getM31());
-        const Vector3d determinantVec = Vector3d(efhi, bchi, bcef);
-
-        return Vector3d::dot(adg, determinantVec);
-#endif // defined(SIMD_SSE2) || defined(SIMD_SSE42)
-    }
-
     Matrix3x3<double>& Matrix3x3<double>::inverse() noexcept
     {
+        // TODO compare those 2 implmenation
+        // the version below look like the best
+#if 0
+        const Vector3d cofactor0 = Vector3d::cross(m_data[1], m_data[2]);
+
+        const double determinant = Vector3d::dot(m_data[0], cofactor0);
+
+        if (fuzzyZero(determinant))
+            return *this;
+
+        const double invDeterminant = (1.0 / determinant);
+        const Vector3d cofactor1 = Vector3d::cross(m_data[2], m_data[0]);
+        const Vector3d cofactor2 = Vector3d::cross(m_data[0], m_data[1]);
+
+        *this = Matrix3x3d(cofactor0, cofactor1, cofactor2).transpose() * (1.0 / determinant);
+
+        return *this;
+#else
+
         // [a, b, c]
         // [d, e, f]
         // [g, h, i]
-
         // compute determoinant from colms 1
         // clang-format off
-        const double efhi = Matrix2x2d(getM22(), getM23(), 
-                                       getM32(), getM33()).determinant(); // m11
+        // TODO look if computing the determinant by calling determinant is not quicker
 
-        const double bchi = Matrix2x2d(getM12(), getM13(), 
-                                       getM32(), getM33()).determinant();  // m21
+        const Vector2d m12m13 = m_data[0].yz();
+        const Vector2d m22m23 = m_data[1].yz();
+        const Vector2d m32m33 = m_data[2].yz();
 
-        const double bcef = Matrix2x2d(getM12(), getM13(),
-                                       getM22(), getM23()).determinant(); // m31
+
+        const double efhi = Matrix2x2d(m22m23, 
+                                       m32m33).determinant(); // m11
+
+        const double bchi = Matrix2x2d(m12m13, 
+                                       m32m33).determinant();  // m21
+
+        const double bcef = Matrix2x2d(m12m13,
+                                       m22m23).determinant(); // m31
 
         const double determinant = [&]()
         {
@@ -60,18 +60,26 @@ namespace MathLib
         // clang-format on
 
         // compute the left element for cofactor
+        const Vector2d m21m23 = m_data[1].xz();
+        const Vector2d m31m33 = m_data[2].xz();
 
-        const double dfgi = Matrix2x2d(getM21(), getM23(), getM31(), getM33()).determinant(); // m12
+        const Vector2d m21m22 = m_data[1].xy();
+        const Vector2d m31m32 = m_data[2].xy();
 
-        const double degh = Matrix2x2d(getM21(), getM22(), getM31(), getM32()).determinant(); // m13
+        const Vector2d m11m13 = m_data[0].xz();
+        const Vector2d m11m12 = m_data[0].xy();
 
-        const double acgi = Matrix2x2d(getM11(), getM13(), getM31(), getM33()).determinant(); // m22
+        const double dfgi = Matrix2x2d(m21m23, m31m33).determinant(); // m12
 
-        const double abgh = Matrix2x2d(getM11(), getM12(), getM31(), getM32()).determinant(); // m23
+        const double degh = Matrix2x2d(m21m22, m31m32).determinant(); // m13
 
-        const double acdf = Matrix2x2d(getM11(), getM13(), getM21(), getM23()).determinant(); // m32
+        const double acgi = Matrix2x2d(m11m13, m31m33).determinant(); // m22
 
-        const double abde = Matrix2x2d(getM11(), getM12(), getM21(), getM22()).determinant(); // m33
+        const double abgh = Matrix2x2d(m11m12, m31m32).determinant(); // m23
+
+        const double acdf = Matrix2x2d(m11m13, m21m23).determinant(); // m32
+
+        const double abde = Matrix2x2d(m11m12, m21m22).determinant(); // m33
 
         // construct the transposed cofactor (adjugate)
         // clang-format off
@@ -83,6 +91,7 @@ namespace MathLib
         // clang-format on
         *this = transposedCofactor * (1.0 / determinant);
         return *this;
+#endif
     }
 
 } // MathLib
--- Source/Matrix4x4d.cpp
+++ Source/Matrix4x4d.cpp
@@ -1,5 +1,8 @@
+#include <MathLibHeader.hpp>
 #include <Matrix4x4d.hpp>
 #include <Matrix3x3d.hpp>
+#include <Vector3d.hpp>
+#include <Vector4d.hpp>
 
 namespace MathLib
 {
