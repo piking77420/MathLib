85c584f4754a7884ddbd79e3db01ad919048ebf6
piking <bon.gabriel77@gmail.com>
2026-08-22 09:54:32 +0200

    Add Vec4 AVX2 permuttation

--- Source/Vector4d.cpp
+++ Source/Vector4d.cpp
@@ -1,6 +1,50 @@
 #include <Vector4d.hpp>
+#include <Vector3d.hpp>
 
 namespace MathLib
 {
+    Vector3<double> Vector4<double>::xyz() const noexcept
+    {
+#if defined(SIMD_AVX)
+#else
+#endif // defined(SIMD_AVX)
+    }
+    Vector3<double> Vector4<double>::xzy() const noexcept
+    {
+#if defined(SIMD_AVX)
+#else
+        return Vector3d(m_x, m_z, m_y);
+#endif // defined(SIMD_AVX)
+    }
+
+    Vector3<double> Vector4<double>::yxz() const noexcept
+    {
+#if defined(SIMD_AVX)
+#else
+        return Vector3d(m_y, m_x, m_z);
+#endif // defined(SIMD_AVX)
+    }
+    Vector3<double> Vector4<double>::yzx() const noexcept
+    {
+#if defined(SIMD_AVX)
+#else
+        return Vector3d(m_y, m_z, m_x);
+#endif // defined(SIMD_AVX)
+    }
+
+    Vector3<double> Vector4<double>::zxy() const noexcept
+    {
+#if defined(SIMD_AVX)
+#else
+        return Vector3d(m_z, m_x, m_y);
+#endif // defined(SIMD_AVX)
+    }
+    Vector3<double> Vector4<double>::zyx() const noexcept
+    {
+#if defined(SIMD_AVX)
+#else
+        return Vector3d(m_z, m_y, m_x);
+#endif // defined(SIMD_AVX)
+    }
 
 } // MathLib
